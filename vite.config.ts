0f272328215ce58fb6f37a44b807135d04486f93
kimzini <kimjinhee.dev@gmail.com>
2025-08-19 20:27:52 +0900

    refactor: vite 프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "@svgr/rollup";
+import { loadEnv } from "vite";
+
 // https://vitejs.dev/config/
+
+const env = loadEnv("", process.cwd(), "");
+
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
@@ -12,5 +17,19 @@ export default defineConfig({
   },
   define: {
     global: "window",
+  },
+  server: {
+    proxy: {
+      "/s3": {
+        target: env.VITE_YAJOBA_S3_PROXY_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/s3/, ""),
+      },
+    "/api": {
+      target: env.VITE_YAJOBA_SERVER_PROXY_URL,
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, "/api"),
+    },
+    },
   },
 });
