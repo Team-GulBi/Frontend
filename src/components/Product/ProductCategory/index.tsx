92c54ad40800abb75d3779b2c6f78b268db2064a
kimzini <kimjinhee.dev@gmail.com>
2025-08-22 01:30:19 +0900

    feat: 상품 등록 기능 재연동

--- src/components/Product/ProductCategory/index.tsx
+++ src/components/Product/ProductCategory/index.tsx
@@ -11,11 +11,10 @@ import { ReactComponent as RightArray } from '@/assets/svgs/rightarray.svg';
 import useGetBCategories, { Category } from '@/apis/useGetBCategories';
 import useGetMCategories from '@/apis/useGetMCategories';
 
-
 interface CategoryDropdownsProps {
-  setBCategoryId: (id: number) => void;
-  setMCategoryId: (id: number) => void;
-  setSCategoryId: (id: number) => void;
+  setBCategoryId: (id: number | undefined) => void;
+  setMCategoryId: (id: number | undefined) => void;
+  setSCategoryId: (id: number | undefined) => void;
   initialBCategoryId?: number;
   initialMCategoryId?: number;
   initialSCategoryId?: number;
@@ -39,144 +38,150 @@ export const CategoryDropdowns = ({
   const [selectedMinorCategory, setSelectedMinorCategory] =
     useState('소분류 선택');
 
-    useEffect(() => {
-      useGetBCategories()
-        .then((response) => setCategories(response.data))
-        .catch((error) => console.error('대분류 조회 실패:', error));
-    }, []);
-  
-    useEffect(() => {
-      if (initialBCategoryId) {
-        const selectedCategory = categories.find((c) => c.id === initialBCategoryId);
-        if (selectedCategory) {
-          setSelectedMainCategory(selectedCategory.name);
-          setBCategoryId(initialBCategoryId);
-          fetchSubCategories(initialBCategoryId);
-        }
-      }
-    }, [categories, initialBCategoryId]);
-  
-    useEffect(() => {
-      if (initialMCategoryId) {
-        const selectedCategory = subCategories.find((c) => c.id === initialMCategoryId);
-        if (selectedCategory) {
-          setSelectedSubCategory(selectedCategory.name);
-          setMCategoryId(initialMCategoryId);
-          fetchMinorCategories(initialMCategoryId);
-        }
-      }
-    }, [subCategories, initialMCategoryId]);
-  
-    useEffect(() => {
-      if (initialSCategoryId) {
-        const selectedCategory = minorCategories.find((c) => c.id === initialSCategoryId);
-        if (selectedCategory) {
-          setSelectedMinorCategory(selectedCategory.name);
-          setSCategoryId(initialSCategoryId);
-        }
-      }
-    }, [minorCategories, initialSCategoryId]);
+  useEffect(() => {
+    useGetBCategories()
+      .then((response) => setCategories(response.data))
+      .catch((error) => console.error('대분류 조회 실패:', error));
+  }, []);
 
-    const fetchSubCategories = async (mainCategoryId: number) => {
-      try {
-        const response = await useGetMCategories(mainCategoryId.toString());
-        setSubCategories(response.data);
-      } catch (error) {
-        console.error('중분류 조회 실패:', error);
+  useEffect(() => {
+    if (initialBCategoryId) {
+      const selectedCategory = categories.find(
+        (c) => c.id === initialBCategoryId,
+      );
+      if (selectedCategory) {
+        setSelectedMainCategory(selectedCategory.name);
+        setBCategoryId(initialBCategoryId);
+        fetchSubCategories(initialBCategoryId);
       }
-    };
-  
-    const fetchMinorCategories = async (subCategoryId: number) => {
-      try {
-        const response = await useGetMCategories(subCategoryId.toString());
-        setMinorCategories(response.data);
-      } catch (error) {
-        console.error('소분류 조회 실패:', error);
-      }
-    };
+    }
+  }, [categories, initialBCategoryId]);
 
-    return (
-      <div className="flex flex-col gap-4 px-[1rem]">
-        <span className="text-medium18 text-neutral-0">상품 카테고리</span>
-        <div className="flex flex-wrap items-center gap-2 md:gap-4 lg:gap-8">
-          {/* 대분류 */}
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button className="font-regular flex min-w-[180px] items-center justify-between border border-neutral-80 bg-neutral-100 px-8 py-6 text-medium20 text-neutral-40">
-                <span>{selectedMainCategory}</span>
-                <Dropdown className="self-center pt-[2px]" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="bg-neutral-100 py-4 text-medium18 font-light text-neutral-30">
-              {categories.map((category) => (
-                <DropdownMenuItem
-                  key={category.id}
-                  onClick={() => {
-                    setSelectedMainCategory(category.name);
-                    setSelectedSubCategory('중분류 선택');
-                    setSelectedMinorCategory('소분류 선택');
-                    setBCategoryId(category.id);
-                    fetchSubCategories(category.id);
-                  }}
-                >
-                  {category.name}
-                </DropdownMenuItem>
-              ))}
-            </DropdownMenuContent>
-          </DropdownMenu>
-  
-          <RightArray />
-  
-          {/* 중분류 */}
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button className="font-regular flex min-w-[180px] items-center justify-between border border-neutral-80 bg-neutral-100 px-8 py-6 text-medium20 text-neutral-40">
-                <span>{selectedSubCategory}</span>
-                <Dropdown className="self-center pt-[2px]"/>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="bg-neutral-100 py-4 text-medium18 font-light text-neutral-30">
-              {subCategories.map((subCategory) => (
-                <DropdownMenuItem
-                  key={subCategory.id}
-                  onClick={() => {
-                    setSelectedSubCategory(subCategory.name);
-                    setSelectedMinorCategory('소분류 선택');
-                    setMCategoryId(subCategory.id);
-                    fetchMinorCategories(subCategory.id);
-                  }}
-                >
-                  {subCategory.name}
-                </DropdownMenuItem>
-              ))}
-            </DropdownMenuContent>
-          </DropdownMenu>
-  
-          <RightArray />
-  
-          {/* 소분류 */}
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button className="font-regular flex min-w-[180px] items-center justify-between border border-neutral-80 bg-neutral-100 px-8 py-6 text-medium20 text-neutral-40">
-                <span>{selectedMinorCategory}</span>
-                <Dropdown className="self-center pt-[2px]"/>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="bg-neutral-100 py-4 text-medium18 font-light text-neutral-30">
-              {minorCategories.map((minorCategory) => (
-                <DropdownMenuItem
-                  key={minorCategory.id}
-                  onClick={() => {
-                    setSelectedMinorCategory(minorCategory.name);
-                    setSCategoryId(minorCategory.id);
-                  }}
-                >
-                  {minorCategory.name}
-                </DropdownMenuItem>
-              ))}
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-      </div>
-    );
+  useEffect(() => {
+    if (initialMCategoryId) {
+      const selectedCategory = subCategories.find(
+        (c) => c.id === initialMCategoryId,
+      );
+      if (selectedCategory) {
+        setSelectedSubCategory(selectedCategory.name);
+        setMCategoryId(initialMCategoryId);
+        fetchMinorCategories(initialMCategoryId);
+      }
+    }
+  }, [subCategories, initialMCategoryId]);
+
+  useEffect(() => {
+    if (initialSCategoryId) {
+      const selectedCategory = minorCategories.find(
+        (c) => c.id === initialSCategoryId,
+      );
+      if (selectedCategory) {
+        setSelectedMinorCategory(selectedCategory.name);
+        setSCategoryId(initialSCategoryId);
+      }
+    }
+  }, [minorCategories, initialSCategoryId]);
+
+  const fetchSubCategories = async (mainCategoryId: number) => {
+    try {
+      const response = await useGetMCategories(mainCategoryId.toString());
+      setSubCategories(response.data);
+    } catch (error) {
+      console.error('중분류 조회 실패:', error);
+    }
   };
+
+  const fetchMinorCategories = async (subCategoryId: number) => {
+    try {
+      const response = await useGetMCategories(subCategoryId.toString());
+      setMinorCategories(response.data);
+    } catch (error) {
+      console.error('소분류 조회 실패:', error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-4 px-[1rem]">
+      <span className="text-medium18 text-neutral-0">상품 카테고리</span>
+      <div className="flex flex-wrap items-center gap-2 md:gap-4 lg:gap-8">
+        {/* 대분류 */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="font-regular flex min-w-[180px] items-center justify-between border border-neutral-80 bg-neutral-100 px-8 py-6 text-medium20 text-neutral-40">
+              <span>{selectedMainCategory}</span>
+              <Dropdown className="self-center pt-[2px]" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="bg-neutral-100 py-4 text-medium18 font-light text-neutral-30">
+            {categories.map((category) => (
+              <DropdownMenuItem
+                key={category.id}
+                onClick={() => {
+                  setSelectedMainCategory(category.name);
+                  setSelectedSubCategory('중분류 선택');
+                  setSelectedMinorCategory('소분류 선택');
+                  setBCategoryId(category.id);
+                  fetchSubCategories(category.id);
+                }}
+              >
+                {category.name}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <RightArray />
+
+        {/* 중분류 */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="font-regular flex min-w-[180px] items-center justify-between border border-neutral-80 bg-neutral-100 px-8 py-6 text-medium20 text-neutral-40">
+              <span>{selectedSubCategory}</span>
+              <Dropdown className="self-center pt-[2px]" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="bg-neutral-100 py-4 text-medium18 font-light text-neutral-30">
+            {subCategories.map((subCategory) => (
+              <DropdownMenuItem
+                key={subCategory.id}
+                onClick={() => {
+                  setSelectedSubCategory(subCategory.name);
+                  setSelectedMinorCategory('소분류 선택');
+                  setMCategoryId(subCategory.id);
+                  fetchMinorCategories(subCategory.id);
+                }}
+              >
+                {subCategory.name}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <RightArray />
+
+        {/* 소분류 */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="font-regular flex min-w-[180px] items-center justify-between border border-neutral-80 bg-neutral-100 px-8 py-6 text-medium20 text-neutral-40">
+              <span>{selectedMinorCategory}</span>
+              <Dropdown className="self-center pt-[2px]" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="bg-neutral-100 py-4 text-medium18 font-light text-neutral-30">
+            {minorCategories.map((minorCategory) => (
+              <DropdownMenuItem
+                key={minorCategory.id}
+                onClick={() => {
+                  setSelectedMinorCategory(minorCategory.name);
+                  setSCategoryId(minorCategory.id);
+                }}
+              >
+                {minorCategory.name}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+};
--- src/pages/Product/Create/page.tsx
+++ src/pages/Product/Create/page.tsx
@@ -1,5 +1,4 @@
-import { ChangeEvent, useState } from 'react';
-import { TagInput } from '@/components/Product/Input/TagInput';
+import { ChangeEvent, useState, useEffect } from 'react';
 import { PriceInput } from '@/components/Product/Input/PriceInput';
 import { LocationInput } from '@/components/Product/Input/LocationInput';
 import { HeaderWithoutSearch } from '@/components/common/Header';
@@ -10,26 +9,110 @@ import usePostProduct from '@/hooks/mutations/usePostProduct';
 import { useNavigate } from 'react-router-dom';
 import RoutePath from '@/routes/routePath';
 import { CreateImageUploader } from '@/components/Product/ImageUploader/Create';
+import { ProductContractTemplate } from '@/components/contract/ProductContractTemplate';
+import { refreshToken } from '@/apis';
+import { ReactComponent as RightArrow } from '@/assets/svgs/RightArrow.svg';
+import { Toast } from '@/components/common/Toast';
 
 const ProductCreatePage = () => {
+  const [step, setStep] = useState<'product' | 'contract'>('product');
+
   const [title, setTitle] = useState('');
   const [name, setName] = useState('');
-  const [tag, setTag] = useState('');
   const [description, setDescription] = useState('');
-  const [price, setPrice] = useState<string>('');
+  const [price, setPrice] = useState<number | undefined>(undefined);
   const [sido, setSido] = useState('');
   const [sigungu, setSigungu] = useState('');
   const [bname, setBname] = useState('');
-  const [bcategoryId, setBCategoryId] = useState<number>(1);
-  const [mcategoryId, setMCategoryId] = useState<number>(2);
-  const [scategoryId, setSCategoryId] = useState<number>(3);
+  const [bcategoryId, setBCategoryId] = useState<number | undefined>(undefined);
+  const [mcategoryId, setMCategoryId] = useState<number | undefined>(undefined);
+  const [scategoryId, setSCategoryId] = useState<number | undefined>(undefined);
   const [images, setImages] = useState<File[]>([]);
   const [mainImage, setMainImage] = useState<File | null>(null);
 
-  const navigate = useNavigate();
-  const { mutate: createProduct, isPending: isUpdating } = usePostProduct();
+  const [toastMessage, setToastMessage] = useState('');
+  const [isToastVisible, setIsToastVisible] = useState(false);
+  const [toastType, setToastType] = useState<'error' | 'success'>('error');
 
-  const handleImageUpload = (imageFiles: File[], mainImageFile: File | null) => {
+  const [contractData, setContractData] = useState({
+    specification: '',
+    condition: '',
+    note: '',
+    rentalPlace: '',
+    returnPlace: '',
+    lateInterestRate: 0,
+    latePenaltyRate: 0,
+    damageCompensationRate: 0,
+  });
+
+  const userName = localStorage.getItem('nickname') || '';
+
+  const navigate = useNavigate();
+
+  const resetForm = () => {
+    setStep('product');
+    setTitle('');
+    setName('');
+    setDescription('');
+    setPrice(undefined);
+    setSido('');
+    setSigungu('');
+    setBname('');
+    setBCategoryId(undefined);
+    setMCategoryId(undefined);
+    setSCategoryId(undefined);
+    setImages([]);
+    setMainImage(null);
+    setContractData({
+      specification: '',
+      condition: '',
+      note: '',
+      rentalPlace: '',
+      returnPlace: '',
+      lateInterestRate: 0,
+      latePenaltyRate: 0,
+      damageCompensationRate: 0,
+    });
+  };
+
+  const { mutate: createProduct } = usePostProduct(
+    () => {
+      setToastMessage('상품이 등록되는 중이에요!');
+      setToastType('success');
+      setIsToastVisible(true);
+
+      setTimeout(() => {
+        navigate(RoutePath.MyPage, { replace: true });
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        resetForm();
+      }, 2000);
+    },
+    () => {
+      setToastMessage('상품 등록에 실패했습니다. 다시 시도해주세요.');
+      setToastType('error');
+      setIsToastVisible(true);
+    },
+  );
+
+  useEffect(() => {
+    const checkAndRefreshToken = async () => {
+      const nickname = localStorage.getItem('nickname');
+      if (!nickname) {
+        try {
+          await refreshToken();
+        } catch (error) {
+          navigate('/login');
+        }
+      }
+    };
+
+    checkAndRefreshToken();
+  }, [navigate]);
+
+  const handleImageUpload = (
+    imageFiles: File[],
+    mainImageFile: File | null,
+  ) => {
     setImages(imageFiles);
     setMainImage(mainImageFile);
   };
@@ -41,67 +124,238 @@ const ProductCreatePage = () => {
     if (name === 'bname') setBname(value);
   };
 
-  const handleSubmit = () => {
-    if (!mainImage) {
-      console.log("대표 이미지")
-      return;
-    }
-    
-    createProduct({
-      tag,
-      title,
-      name,
-      price,
-      sido,
-      sigungu,
-      bname,
-      description,
-      bcategoryId,
-      mcategoryId,
-      scategoryId,
-      images,
-      mainImage,
-    });
-
-    navigate(RoutePath.MyPage);
+  const handlePrevStep = () => {
+    setStep('product');
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
-  if (isUpdating) {
-    return "로딩중";
-  }
+  const handleNextStep = () => {
+    const errors = [];
+
+    if (!title.trim()) errors.push('제목을 입력해주세요.');
+    if (!name.trim()) errors.push('상품명을 입력해주세요.');
+    if (!price) errors.push('가격을 입력해주세요.');
+    if (!sido.trim()) errors.push('학교를 입력해주세요.');
+    if (!sigungu.trim()) errors.push('건물을 입력해주세요.');
+    if (!bname.trim()) errors.push('상세주소를 입력해주세요.');
+    if (!description.trim()) errors.push('상품 설명을 입력해주세요.');
+    if (!mainImage) {
+      errors.push('대표 이미지를 선택해주세요.');
+    }
+    if (!bcategoryId) errors.push('대분류를 선택해주세요.');
+    if (!mcategoryId) errors.push('중분류를 선택해주세요.');
+    if (!scategoryId) errors.push('소분류를 선택해주세요.');
+
+    if (errors.length > 0) {
+      setToastMessage(errors[0]);
+      setToastType('error');
+      setIsToastVisible(true);
+      return;
+    }
+
+    setStep('contract');
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const handleSubmit = () => {
+    const errors = [];
+
+    if (!contractData.specification.trim())
+      errors.push('상품 규격을 입력해주세요.');
+    if (!contractData.condition.trim())
+      errors.push('상품 상태를 입력해주세요.');
+    if (!contractData.rentalPlace.trim())
+      errors.push('대여 장소를 입력해주세요.');
+    if (!contractData.returnPlace.trim())
+      errors.push('반납 장소를 입력해주세요.');
+    if (
+      contractData.lateInterestRate === undefined ||
+      contractData.lateInterestRate < 0
+    )
+      errors.push('연체 이자율을 입력해주세요.');
+    if (
+      contractData.latePenaltyRate === undefined ||
+      contractData.latePenaltyRate < 0
+    )
+      errors.push('연체 위약금율을 입력해주세요.');
+    if (
+      contractData.damageCompensationRate === undefined ||
+      contractData.damageCompensationRate < 0
+    )
+      errors.push('손해 배상율을 입력해주세요.');
+
+    if (errors.length > 0) {
+      setToastMessage(errors[0]);
+      setToastType('error');
+      setIsToastVisible(true);
+      return;
+    }
+
+    if (
+      mainImage &&
+      bcategoryId &&
+      mcategoryId &&
+      scategoryId &&
+      sido.trim() &&
+      sigungu.trim() &&
+      bname.trim() &&
+      price
+    ) {
+      createProduct({
+        product: {
+          title,
+          name,
+          price,
+          sido,
+          sigungu,
+          bname,
+          description,
+          bcategoryId,
+          mcategoryId,
+          scategoryId,
+        },
+        template: {
+          specification: contractData.specification,
+          condition: contractData.condition,
+          note: contractData.note,
+          rentalPlace: contractData.rentalPlace,
+          returnPlace: contractData.returnPlace,
+          lateInterestRate: contractData.lateInterestRate,
+          latePenaltyRate: contractData.latePenaltyRate,
+          damageCompensationRate: contractData.damageCompensationRate,
+        },
+        images,
+        mainImage,
+      });
+    }
+  };
+
+  const handleContractChange = (data: any) => {
+    setContractData(data);
+  };
 
   return (
     <div className="min-h-screen flex w-screen">
+      <Toast
+        message={toastMessage}
+        isVisible={isToastVisible}
+        onClose={() => setIsToastVisible(false)}
+        type={toastType}
+      />
       <HeaderWithoutSearch />
       <div className="flex w-full flex-col px-[240px] py-[60px]">
-        <div className="flex w-full border-b px-[1rem] pb-[2rem] pt-[4rem] text-xxlarge32 font-semibold">
+        <div className="flex w-full border-b px-2 pb-[2rem] pt-[4rem] text-xxlarge32 font-semibold">
           상품 등록하기
         </div>
         <div className="flex w-full flex-col gap-[2rem] px-[0.5rem] py-[2rem]">
-          <ProductInput title="제목" value={title} onChange={(e) => setTitle(e.target.value)} />
-          <ProductInput title="상품명" value={name} onChange={(e) => setName(e.target.value)} />
-          <TagInput onTagsChange={setTag} />
-          <CreateImageUploader
-            maxImages={10}
-            onImagesChange={handleImageUpload}
-          />
-          <PriceInput value={price} onChangePrice={(e) => setPrice(e.target.value)} />
-          <CategoryDropdowns 
-            setBCategoryId={setBCategoryId}
-            setMCategoryId={setMCategoryId}
-            setSCategoryId={setSCategoryId}
-          />
-          <ProductContent value={description} onChange={(e) => setDescription(e.target.value)} />
-          <LocationInput
-              title="위치"
-              sido={sido}
-              sigungu={sigungu}
-              bname={bname}
-              onChange={handleLocationChange}
-            />
-          <button onClick={handleSubmit} className="mx-[1rem] mb-[5rem] mt-6 bg-secondary-100 px-4 py-3 rounded-[6px] text-white">
-            상품 등록
-          </button>
+          {step === 'product' ? (
+            <>
+              <div className="mb-2 flex flex-col gap-2">
+                <h2 className="text-large24 font-semibold text-secondary-100">
+                  상품 입력
+                </h2>
+                <span className="text-xsmall14 font-medium text-neutral-0">
+                  상품 상세 정보들을 입력해주세요.
+                </span>
+              </div>
+              <ProductInput
+                title="제목"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <ProductInput
+                title="상품명"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <CreateImageUploader
+                maxImages={10}
+                onImagesChange={handleImageUpload}
+                initialImages={images}
+                initialMainImage={mainImage}
+              />
+              <PriceInput value={price} onChangePrice={setPrice} />
+              <CategoryDropdowns
+                setBCategoryId={setBCategoryId}
+                setMCategoryId={setMCategoryId}
+                setSCategoryId={setSCategoryId}
+                initialBCategoryId={bcategoryId}
+                initialMCategoryId={mcategoryId}
+                initialSCategoryId={scategoryId}
+              />
+              <ProductContent
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+              <LocationInput
+                title="위치"
+                sido={sido}
+                sigungu={sigungu}
+                bname={bname}
+                onChange={handleLocationChange}
+              />
+              <div className="flex justify-end">
+                <button
+                  onClick={handleNextStep}
+                  className="flex flex-col items-center gap-2 px-6 py-4"
+                >
+                  <RightArrow
+                    className="text-secondary-100"
+                    width="30"
+                    height="28"
+                    viewBox="0 0 12 10"
+                  />
+                  <span className="text-medium18 font-medium text-secondary-100">
+                    다음 단계
+                  </span>
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="mb-4">
+                <h2 className="mb-2 text-large24 font-semibold text-secondary-100">
+                  계약서 템플릿 작성
+                </h2>
+                <p className="text-xsmall14 font-medium text-neutral-0">
+                  계약서에 들어갈 기본 정보를 입력해주세요.
+                </p>
+                <p className="text-xsmall14 font-medium text-neutral-0">
+                  색칠된 부분은 추후에 대여 신청자가 작성할 내용이에요!
+                </p>
+              </div>
+              <ProductContractTemplate
+                {...contractData}
+                onInputChange={handleContractChange}
+                userName={userName}
+                productName={name}
+                address={`${sido} ${sigungu} ${bname}`}
+                price={price}
+              />
+              <div className="flex items-center justify-between">
+                <button
+                  onClick={handlePrevStep}
+                  className="flex flex-col items-center gap-2 rounded-[6px] px-6 py-4"
+                >
+                  <RightArrow
+                    className="rotate-180 text-secondary-100"
+                    width="30"
+                    height="28"
+                    viewBox="0 0 12 10"
+                  />
+                  <span className="text-medium18 font-medium text-secondary-100">
+                    이전 단계
+                  </span>
+                </button>
+                <button
+                  onClick={handleSubmit}
+                  className="rounded-[6px] bg-secondary-100 px-6 py-3 text-white transition-colors hover:bg-secondary-80"
+                >
+                  상품 등록
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
